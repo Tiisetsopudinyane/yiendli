2a9def393828b5ab637ece16a12762e3cc531684
TIISESTOPUDINYANE <pudinyanesamueltiisetos@gmail.com>
2020-01-30 15:40:30 +0300

    testing web

---
+++ javascript.js
@@ -0,0 +1,7 @@
+
+function geteverything(){
+  var name=document.getElementById('nametext').value;
+  var email=document.getElementById('email').value;
+  var message=document.getElementById('messagetext').value
+  alert(name+" "+ email+" " + message);
+}
